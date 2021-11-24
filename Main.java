99c3d0cd59d6a3d6050295ba252927edd35d56d1
Sanket-mahale <94188679+Sanket-mahale@users.noreply.github.com>
2021-11-24 23:13:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        FrameworkProperties frameworkProperties = new FrameworkProperties();
+        DriverSingleton.getInstance(frameworkProperties.getProperty("browser"));
+        WebDriver driver = DriverSingleton.getDriver();
+        driver.get("http://automationpractice.com");
+    }
+}
